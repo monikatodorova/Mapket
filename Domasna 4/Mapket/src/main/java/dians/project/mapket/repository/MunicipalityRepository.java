bd9d23af1e64f996a6f5f6fce9aff80299aac6e2
Monika <monikatodorova2001@hotmail.com>
2023-02-15 18:52:23 +0100

    Final version

--- Domasna 4/Mapket/src/main/java/dians/project/mapket/repository/MunicipalityRepository.java
+++
@@ -1,8 +0,0 @@
-package dians.project.mapket.repository;
-
-import dians.project.mapket.model.Market;
-import dians.project.mapket.model.Municipality;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MunicipalityRepository extends JpaRepository<Municipality, Long> {
-}
